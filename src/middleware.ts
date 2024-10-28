ebbe91541ab39ed657eafbf69e0388ec313e211f
Areeb ur Rub <areeburrub@gmail.com>
2024-10-28 18:27:03 +0530

    add: /reminders to secured routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher  } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/home(.*)', '/events(.*)'])
+const isProtectedRoute = createRouteMatcher(['/home(.*)', '/events(.*)', '/reminders'])
 
 export default clerkMiddleware(async (auth, req) => {
     if (isProtectedRoute(req)) await auth.protect()
